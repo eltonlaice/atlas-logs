2a319d077f5d06f287b87c782b18dd94aa55abe6
Elton Tomás Laice <me@eltonlaice.com>
2024-09-28 18:19:14 +0200

    Remove variables in /search

--- app/routes/search.py
+++ app/routes/search.py
@@ -7,14 +7,8 @@ search_bp = Blueprint('search', __name__)
 def search_logs():
     if request.method == 'POST':
         query = request.form.get('query', '')
-        start_date = request.form.get('start_date')
-        end_date = request.form.get('end_date')
-        limit = int(request.form.get('limit', 100))
     else:
         query = request.args.get('query', '')
-        start_date = request.args.get('start_date')
-        end_date = request.args.get('end_date')
-        limit = int(request.args.get('limit', 100))
     
     # results = perform_search(query, start_date, end_date, limit)
     
